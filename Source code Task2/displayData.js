3bae095a3562d4abc8058b6f7fa264570f5bf5b5
akmlmhd <116746162+akmlmhd@users.noreply.github.com>
2023-06-07 10:39:53 +0800

    Delete Source code Task2 directory

--- Source code Task2/displayData.js
+++
@@ -1,7 +0,0 @@
-function displayData(jsonData) {
-    var attributes = Object.keys(jsonData[0]);
-    console.log("Attributes in each record: " + attributes.join(", "));
-
-    // Rest of the code to display the data in a table
-    // ...
-}
